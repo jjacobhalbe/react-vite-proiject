35be2675a43315178986af32290f86569623d23a
jjacobhalbe <122291074+jjacobhalbe@users.noreply.github.com>
2025-04-06 11:28:55 +0200

    files fixed

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
@@ -7,6 +8,7 @@ export default defineConfig({
       fastRefresh: true,
       include: '**/*.jsx',
     }),
+    tailwindcss(),
   ],
   server: {
     watch: {
