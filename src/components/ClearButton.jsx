fe0750671fc22edfd468ae69567aa5b5b2afeda7
jjacobhalbe <122291074+jjacobhalbe@users.noreply.github.com>
2025-06-09 14:31:55 +0200

    whoops now clear button added

--- src/components/ClearButton.jsx
+++ src/components/ClearButton.jsx
@@ -1,9 +1,5 @@
 const ClearButton = ({ onClick }) => {
-  return (
-    <button className="checkTheLevel" onClick={onClick}>
-      Check the Level
-    </button>
-  )
+  return <button onClick={onClick}>Check the Level</button>
 }
 
 export default ClearButton
